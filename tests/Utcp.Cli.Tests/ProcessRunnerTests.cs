af18f192e31573f63ec1a2627d3ce0e738f39221
Cicatriz <thedawg100@gmail.com>
2025-08-28 12:54:39 -0400

    test(cli): make ProcessRunnerTests cross-platform by using OS-specific echo

--- tests/Utcp.Cli.Tests/ProcessRunnerTests.cs
+++ tests/Utcp.Cli.Tests/ProcessRunnerTests.cs
@@ -4,18 +4,20 @@
 using Utcp.Cli;
 using FluentAssertions;
 using Xunit;
+using System.Runtime.InteropServices;
 
 public class ProcessRunnerTests
 {
     [Fact]
     public async Task EchoCommand_Works()
     {
+        var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
         var template = new CliCallTemplate
         {
             CallTemplateType = "cli",
             Name = "manual",
-            Command = "/bin/echo",
-            Args = new []{"hello"},
+            Command = isWindows ? "cmd" : "/bin/echo",
+            Args = isWindows ? new []{"/c", "echo", "hello"} : new []{"hello"},
         };
 
         var (code, stdOut, stdErr) = await ProcessRunner.RunAsync(template, default);
